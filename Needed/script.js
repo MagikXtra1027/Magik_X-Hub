6695b22a0f03c1b2c66ffbf23c1d16412ee7026e
Ashton Miller <MagikXPlayz@gmail.com>
2024-06-01 22:21:02 -0400

    added themes using js

--- Needed/script.js
+++ Needed/script.js
@@ -1,9 +1,40 @@
 window.addEventListener("load", () => {
-const loader = document.querySelector(".loader");
+    const loader = document.querySelector(".loader");
 
-loader.classList.add("loader-hidden");
+    loader.classList.add("loader-hidden");
 
-loader.addEventListener("transitionend", () => {
-document.body.removeChild("loader");
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild("loader");
+    });
+
+
+
+    const primaryColor = window.localStorage.getItem("primaryColor");
+    const root = document.querySelector(':root');
+    let colorToSet = 'magenta';
+
+    if (primaryColor === 'blue') {
+        colorToSet = 'blue';
+    }
+    else if (primaryColor === 'green') {
+        colorToSet = 'green';
+    }
+
+    root.style.setProperty('--primary-color', colorToSet);
 })
-})
+
+function toggleTheme() {
+    const primaryColor = window.localStorage.getItem("primaryColor");
+    let colorToSet;
+
+    if (primaryColor === 'magenta') {
+        colorToSet = 'blue';
+    }
+    else {
+        colorToSet = 'magenta';
+    }
+
+    window.localStorage.setItem('primaryColor', colorToSet);
+
+    window.location.reload();
+}
